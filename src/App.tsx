4dc0be6421bb74f91ea276f9b6c3bd0f1808bc32
Santi-Rubiol <santirubiol14@gmail.com>
2025-05-16 15:03:25 -0300

    Quito posibilidad de pasarse con scroll laterl

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import TarjetaCasamiento from './pages/TarjetaCasamiento/TarjetaCasamiento'
 
 export default function App() {
-  return <TarjetaCasamiento />
+  return (
+    <div>
+      <TarjetaCasamiento />
+    </div>
+  )
 }
